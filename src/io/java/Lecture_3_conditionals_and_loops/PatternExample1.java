12c069c2b103441b3d2ed23fc8789b405f42ad2d
mauryapiyush76 <47456936+mauryapiyush76@users.noreply.github.com>
2021-11-25 00:29:21 +0530

    pattern problems

---
+++ src/io/java/Lecture_3_conditionals_and_loops/PatternExample1.java
@@ -0,0 +1,35 @@
+package io.java.Lecture_3_conditionals_and_loops;
+
+import java.util.Scanner;
+
+public class PatternExample1 {
+
+	/*
+	 * Print this pattern
+	 * 
+	 * 1 2 3 4 5 6 7 8 9 10
+	 */
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+
+		int n = in.nextInt();
+
+		in.close();
+
+		int i = 1, k = 1;
+
+		while (i <= n) {
+			int j = 1;
+			while (j <= i) {
+				System.out.print(k + " ");
+				k = k + 1;
+				j = j + 1;
+			}
+			System.out.println();
+			i = i + 1;
+		}
+	}
+
+}
