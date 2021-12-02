5064bced874577749b9f5ac521502eefe426928e
mauryapiyush76 <47456936+mauryapiyush76@users.noreply.github.com>
2021-12-02 22:49:09 +0530

    arrays

---
+++ src/io/java/Lecture_6_arrays/ArrangeNumbersInTheArray.java
@@ -0,0 +1,77 @@
+package io.java.Lecture_6_arrays;
+
+import java.util.Scanner;
+
+public class ArrangeNumbersInTheArray {
+
+	/*
+	Coding Problem
+	Problem Name: Arrange Numbers in the Array
+	Problem Level: EASY
+	Problem Description: #### You have been given an empty array(ARR) and its size N. The only input taken from the user will be N and you need not worry about the array. 
+	#### Your task is to populate the array using the integer values in the range 1 to N(both inclusive) in the order - 1,3,.......4,2. 
+
+	##### Note: 
+	    You need not print the array. You only need to populate it.
+
+
+	##### Input Format :
+	    The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+	    The first and the only line of each test case or query contains an integer 'N'.
+
+	##### Output Format :
+	    For each test case, print the elements of the arra/listy separated by a single space.
+
+	    Output for every test case will be printed in a separate line.
+
+	##### Constraints :
+	    1 <= t <= 10^2
+	    0 <= N <= 10^4
+
+	    Time Limit: 1sec
+	 ##### Sample Input 1 : 
+	    1
+	    6
+	##### Sample Output 1 :
+	    1 3 5 6 4 2
+
+	##### Sample Input 2 : 
+	    2
+	    9
+	    3
+	##### Sample Output 2 :
+	    1 3 5 7 9 8 6 4 2
+	    1 3 2
+	    */
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t, n;
+		t = in.nextInt();
+		while( t-- > 0 ) {
+			n = in.nextInt();
+			int[] arr = new int[n];
+			int left = 0, right = n - 1;
+			int mid = n/2;
+			int odd = 1;
+			while(left <= mid) {
+				arr[left] = 2*odd - 1;
+				left++;
+				odd++;
+			}
+			int even = 1;
+			while(right > mid) {
+				arr[right] = even*2;
+				right--;
+				even++;
+			}
+			for(int i = 0 ; i < n ; i++) {
+				System.out.print(arr[i] + " ");
+			}
+			System.out.println();
+		}
+		in.close();
+		return;
+	}
+}
---
+++ src/io/java/Lecture_6_arrays/ReturnArraySum.java
@@ -0,0 +1,69 @@
+package io.java.Lecture_6_arrays;
+
+import java.util.Scanner;
+
+public class ReturnArraySum {
+
+	/*
+	Coding Problem
+	Problem Name: Return array Sum
+	Problem Level: EASY
+	Problem Description: #### Given an array/list(ARR) of length N, you need to find and return the sum of all the elements in the array/list. 
+
+	##### Input Format :
+	    The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+	    The first line of each test case or query contains an integer 'N' representing the size of the array/list.
+	    
+	    Second line contains 'N' single space separated integers representing the elements in the array/list.
+
+	##### Output Format :
+	    For each test case, print the sum of the numbers in the array/list.
+
+	    Output for every test case will be printed in a separate line.
+
+	##### Constraints :
+	    1 <= t <= 10^2
+	    0 <= N <= 10^5
+
+	    Time Limit: 1sec
+
+	 ##### Sample Input 1: 
+	    1
+	    3
+	    9 8 9
+
+	##### Sample Output 1:
+	    26
+
+	##### Sample Input 2: 
+	    2
+	    5
+	    1 2 3 4 5 
+	    3
+	    10 20 30 
+
+	##### Sample Output 2:
+	    15
+	    60
+	    */
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t, n;
+		t = in.nextInt();
+		while( t-- > 0 ) {
+			n = in.nextInt();
+			int[] arr = new int[n];
+			int sum = 0;
+			for(int i = 0 ; i < n ; i++) {
+				arr[i] = in.nextInt();
+				sum += arr[i]; 
+			}
+			System.out.println(sum);
+		}
+		in.close();
+		return;
+	}
+	
+}
